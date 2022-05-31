01db641088b4feae363745f6515e25492e28178d
jmooremcc <jmooremcc@hotmail.com>
2022-05-31 11:27:02 -0400

    FixedPoint Math Class Version 1.2 Refined math operations to eliminate unnecessary multiplication or division operations Added the ability to perform math operations in any order. Also added Fractions and other methods that allows the module to work with sum and mean functions when FixedPoint objects are used in a list

--- FixedPoint.py
+++ FixedPoint.py
@@ -1,7 +1,32 @@
 #FixedPoint Math Module
 # by jmooremcc
 
-__VERSION__ = 1.1
+from fractions import Fraction
+
+__VERSION__ = 1.2
+"""
+FixedPoint Module
+Performs fixed point arithmetic suitable for Currency operations
+Examples:
+    qty = FixedPoint(5)
+    price = FixedPoint(0.69)
+    total = qty * price
+    
+Change Log
+    Version 1.0:
+        Initial version
+        
+    Version 1.1:
+        Streamlined operations to perform math operations using internal integer representation
+        Minor bug fixes
+        
+    Version 1.2:
+        Refined math operations to eliminate unnecessary multiplication or division
+        added the ability to perform math operations in any order.
+        Also added Fractions and other methods that allow the module to work with sum and mean functions
+        when FixedPoint objects are used in a list
+    
+"""
 
 class FixedPoint:
     def __init__(self, value, FPV=False):
@@ -13,10 +38,27 @@ class FixedPoint:
             self._value = value * 100
         elif type(value) == FixedPoint:
             self._value = value._value
+        else:
+            self._value = int(value * 100)
+
+        self.fraction = Fraction(self.value)
 
     @property
     def value(self):
         return self._value / 100
+
+    @value.setter
+    def value(self, val):
+        self._value = val
+        self.fraction = Fraction(self.value)
+
+    @property
+    def numerator(self):
+        return self.fraction.numerator
+
+    @property
+    def denominator(self):
+        return self.fraction.denominator
 
     def __add__(self, other):
         if type(other) == FixedPoint:
@@ -25,7 +67,7 @@ class FixedPoint:
             return FixedPoint(self.value + other)
 
     def __iadd__(self, other):
-        self._value = (self.__add__(other))._value
+        self.value = (self.__add__(other))._value
         return self
 
     def __sub__(self, other):
@@ -35,17 +77,17 @@ class FixedPoint:
             return FixedPoint(self.value - other)
 
     def __isub__(self, other):
-        self._value = (self.__sub__(other))._value
+        self.value = (self.__sub__(other))._value
         return self
 
     def __mul__(self, other):
         if type(other) == FixedPoint:
-            return FixedPoint(self._value * other._value/100, True)
+            return FixedPoint(self._value * other.value, True)
         else:
             return FixedPoint(self.value * other)
 
     def __imul__(self, other):
-        self._value = (self.__mul__(other))._value
+        self.value = (self.__mul__(other))._value
         return self
 
     def __truediv__(self, other):
@@ -55,8 +97,30 @@ class FixedPoint:
             return FixedPoint(self.value / other)
 
     def __itruediv__(self, other):
-        self._value = (self.__truediv__(other))._value
+        self.value = (self.__truediv__(other))._value
         return self
+
+    def __floordiv__(self, other):
+        if type(other) == FixedPoint:
+            return FixedPoint(self._value // other._value)
+        else:
+            return FixedPoint(self.value // other)
+
+    def __ifloordiv__(self, other):
+        self.value = (self.__floordiv__(other))._value
+        return self
+
+    def __radd__(self, other):
+        return self.value + other
+
+    def __rsub__(self, other):
+        return self.value - other
+
+    def __rmul__(self, other):
+        return self.value * other
+
+    def __rdiv__(self, other):
+        return self.value / other
 
     def __eq__(self, other):
         if type(other) == FixedPoint:
@@ -78,9 +142,23 @@ class FixedPoint:
         if type(other) == FixedPoint:
             return self._value >= other._value
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}: {self.value}"
+    def __cmp__(self, other):
+        return int(self.value - other)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.value:0.2f})"
+
+    def __str__(self):
+        return str(self.value)
+
+    def __int__(self):
+        return int(self.value)
+
+    def __float__(self):
+        return self.value
+
+    def __coerce__(self, other):
+        return (int(self.value), int(other))
 
 
 if __name__ == "__main__":
@@ -106,6 +184,9 @@ if __name__ == "__main__":
 
     value = qty / FixedPoint(3)
     print(f"{value=}")
+
+    qty /= FixedPoint(2)
+    print(f"{qty=}")
 
     print(FixedPoint(0.1) + FixedPoint(0.1) + FixedPoint(0.1))
 
